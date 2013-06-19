695c6fc26a988dc3761ceb9a4651451bc1b50f9b
Zachary Seymour <adamsey16@gmail.com>
2013-06-19 14:34:30 -0400

    Testing ObjectDatabase and db connection

--- test.cpp
+++ test.cpp
@@ -5,12 +5,51 @@
  *      Author: zachary
  */
 #include <iostream>
+#include "DatabaseManager.h"
+#include "ObjectDatabase.h"
+#include "dbwrappers/Database.h"
+#include <stdio.h>
+#include <sqlite3.h>
+#include "utils/StdString.h"
+#include "utils/StringUtils.h"
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!";
-	return 0;
+	CStdString host = "/home/zachary/workspace/xbmc-database-4.0/Debug";
+	//CStdString file = "database4.db";
+	DatabaseSettings settings;
+	settings.host = host;
+	settings.name = "database";
+	settings.type = "sqlite3";
+
+	CDatabaseManager::Get().Initialize(false,&settings);
+
+	CObjectDatabase db;
+	if(db.Open())
+	{
+		vector<int> ids;
+		if(db.GetAllAttributesForObjectType(16,ids))
+		{
+//			for(vector<int>::iterator it = ids.begin(); it != ids.end(); ++it)
+//			{
+//				cout << *it << endl;
+//			}
+
+			cout << StringUtils::Join(ids,",");
+		}
+	}
+
+//	sqlite3 *db;
+//
+//	int rc;
+//	rc = sqlite3_open(host + "/" + file, &db);
+//	if( rc ){
+//		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+//		sqlite3_close(db);
+//		return(1);
+//	}
+
 }
 
 
