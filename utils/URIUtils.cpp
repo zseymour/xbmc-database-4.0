f9a4771ed26d7b6e8d15c8716ebd4f866554d88f
Zachary Seymour <adamsey16@gmail.com>
2013-06-24 12:36:39 -0400

    Added methods for splitting files and some more is***()

--- utils/URIUtils.cpp
+++ utils/URIUtils.cpp
@@ -6,7 +6,9 @@
  */
 
 #include "URIUtils.h"
+#include "StringUtils.h"
 #include "../URL.h"
+#include <cstring>
 
 using namespace std;
 
@@ -308,6 +310,291 @@ bool URIUtils::CompareWithoutSlashAtEnd(const CStdString& strPath1, const CStdSt
   return strc1.Equals(strc2);
 }
 
+void URIUtils::Split(const CStdString& strFileNameAndPath,
+                     CStdString& strPath, CStdString& strFileName)
+{
+  //Splits a full filename in path and file.
+  //ex. smb://computer/share/directory/filename.ext -> strPath:smb://computer/share/directory/ and strFileName:filename.ext
+  //Trailing slash will be preserved
+  strFileName = "";
+  strPath = "";
+  int i = strFileNameAndPath.size() - 1;
+  while (i > 0)
+  {
+    char ch = strFileNameAndPath[i];
+    // Only break on ':' if it's a drive separator for DOS (ie d:foo)
+    if (ch == '/' || ch == '\\' || (ch == ':' && i == 1)) break;
+    else i--;
+  }
+  if (i == 0)
+    i--;
+
+  strPath = strFileNameAndPath.Left(i + 1);
+  strFileName = strFileNameAndPath.Right(strFileNameAndPath.size() - i - 1);
+}
+
+CStdStringArray URIUtils::SplitPath(const CStdString& strPath)
+{
+  CURL url(strPath);
+
+  // silly CStdString can't take a char in the constructor
+  CStdString sep(1, url.GetDirectorySeparator());
+
+  // split the filename portion of the URL up into separate dirs
+  CStdStringArray dirs;
+  StringUtils::SplitString(url.GetFileName(), sep, dirs);
+
+  // we start with the root path
+  CStdString dir = url.GetWithoutFilename();
+
+  if (!dir.IsEmpty())
+    dirs.insert(dirs.begin(), dir);
+
+  // we don't need empty token on the end
+  if (dirs.size() > 1 && dirs.back().IsEmpty())
+    dirs.erase(dirs.end() - 1);
+
+  return dirs;
+}
+
+bool URIUtils::IsOnDVD(const CStdString& strFile)
+{
+#ifdef _WIN32
+  if (strFile.Mid(1,1) == ":")
+    return (GetDriveType(strFile.Left(2)) == DRIVE_CDROM);
+#endif
+
+  if (strFile.Left(4).CompareNoCase("dvd:") == 0)
+    return true;
+
+  if (strFile.Left(4).CompareNoCase("udf:") == 0)
+    return true;
+
+  if (strFile.Left(8).CompareNoCase("iso9660:") == 0)
+    return true;
+
+  if (strFile.Left(5).CompareNoCase("cdda:") == 0)
+    return true;
+
+  return false;
+}
+
+bool URIUtils::IsMultiPath(const CStdString& strPath)
+{
+  return strPath.Left(10).Equals("multipath:");
+}
+
+bool URIUtils::IsDVD(const CStdString& strFile)
+{
+  CStdString strFileLow = strFile;
+  strFileLow.MakeLower();
+  if (strFileLow.Find("video_ts.ifo") != -1 && IsOnDVD(strFile))
+    return true;
+
+#if defined(_WIN32)
+  if (strFile.Left(6).Equals("dvd://"))
+    return true;
+
+  if(strFile.Mid(1) != ":\\"
+  && strFile.Mid(1) != ":")
+    return false;
+
+  if(GetDriveType(strFile.c_str()) == DRIVE_CDROM)
+    return true;
+#else
+  if (strFileLow == "iso9660://" || strFileLow == "udf://" || strFileLow == "dvd://1" )
+    return true;
+#endif
+
+  return false;
+}
+
+bool URIUtils::IsStack(const CStdString& strFile)
+{
+  return strFile.Left(6).Equals("stack:");
+}
+
+bool URIUtils::IsRAR(const CStdString& strFile)
+{
+  CStdString strExtension = GetExtension(strFile);
+
+  if (strExtension.Equals(".001") && strFile.Mid(strFile.length()-7,7).CompareNoCase(".ts.001"))
+    return true;
+
+  if (strExtension.CompareNoCase(".cbr") == 0)
+    return true;
+
+  if (strExtension.CompareNoCase(".rar") == 0)
+    return true;
+
+  return false;
+}
+
+bool URIUtils::IsInArchive(const CStdString &strFile)
+{
+  return IsInZIP(strFile) || IsInRAR(strFile) || IsInAPK(strFile);
+}
+
+bool URIUtils::IsInAPK(const CStdString& strFile)
+{
+  CURL url(strFile);
+
+  return url.GetProtocol() == "apk" && url.GetFileName() != "";
+}
+
+bool URIUtils::IsInZIP(const CStdString& strFile)
+{
+  CURL url(strFile);
+
+  return url.GetProtocol() == "zip" && url.GetFileName() != "";
+}
+
+bool URIUtils::IsInRAR(const CStdString& strFile)
+{
+  CURL url(strFile);
+
+  return url.GetProtocol() == "rar" && url.GetFileName() != "";
+}
+
+bool URIUtils::IsAPK(const CStdString& strFile)
+{
+  return GetExtension(strFile).CompareNoCase(".apk") == 0;
+}
+
+bool URIUtils::IsZIP(const CStdString& strFile) // also checks for comic books!
+{
+  CStdString strExtension = GetExtension(strFile);
+
+  if (strExtension.CompareNoCase(".zip") == 0)
+    return true;
+
+  if (strExtension.CompareNoCase(".cbz") == 0)
+    return true;
+
+  return false;
+}
+
+bool URIUtils::IsArchive(const CStdString& strFile)
+{
+  CStdString extension = GetExtension(strFile);
+
+  return (extension.CompareNoCase(".zip") == 0 ||
+          extension.CompareNoCase(".rar") == 0 ||
+          extension.CompareNoCase(".apk") == 0 ||
+          extension.CompareNoCase(".cbz") == 0 ||
+          extension.CompareNoCase(".cbr") == 0);
+}
+
+bool URIUtils::IsPlugin(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("plugin");
+}
+
+bool URIUtils::IsScript(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("script");
+}
+
+bool URIUtils::IsAddonsPath(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("addons");
+}
+
+bool URIUtils::IsSourcesPath(const CStdString& strPath)
+{
+  CURL url(strPath);
+  return url.GetProtocol().Equals("sources");
+}
+
+bool URIUtils::IsCDDA(const CStdString& strFile)
+{
+  return strFile.Left(5).Equals("cdda:");
+}
+
+bool URIUtils::IsISO9660(const CStdString& strFile)
+{
+  return strFile.Left(8).Equals("iso9660:");
+}
+
+bool URIUtils::IsURL(const CStdString& strFile)
+{
+  return strFile.Find("://") >= 0;
+}
+
+bool URIUtils::IsDAAP(const CStdString& strFile)
+{
+  return strFile.Left(5).Equals("daap:");
+}
+
+bool URIUtils::IsUPnP(const CStdString& strFile)
+{
+  return strFile.Left(5).Equals("upnp:");
+}
+
+bool URIUtils::IsTuxBox(const CStdString& strFile)
+{
+  return strFile.Left(7).Equals("tuxbox:");
+}
+
+bool URIUtils::IsMythTV(const CStdString& strFile)
+{
+  return strFile.Left(5).Equals("myth:");
+}
+
+bool URIUtils::IsHDHomeRun(const CStdString& strFile)
+{
+  return strFile.Left(10).Equals("hdhomerun:");
+}
+
+bool URIUtils::IsSlingbox(const CStdString& strFile)
+{
+  return strFile.Left(6).Equals("sling:");
+}
+
+bool URIUtils::IsVTP(const CStdString& strFile)
+{
+  return strFile.Left(4).Equals("vtp:");
+}
+
+bool URIUtils::IsHTSP(const CStdString& strFile)
+{
+  return strFile.Left(5).Equals("htsp:");
+}
+
+
+
+bool URIUtils::IsPVRRecording(const CStdString& strFile)
+{
+  CStdString strFileWithoutSlash(strFile);
+  RemoveSlashAtEnd(strFileWithoutSlash);
+
+  return strFileWithoutSlash.Right(4).Equals(".pvr") &&
+         strFile.Left(16).Equals("pvr://recordings");
+}
+
+bool URIUtils::IsMusicDb(const CStdString& strFile)
+{
+  return strFile.Left(8).Equals("musicdb:");
+}
+
+bool URIUtils::IsVideoDb(const CStdString& strFile)
+{
+  return strFile.Left(8).Equals("videodb:");
+}
+
+bool URIUtils::IsBluray(const CStdString& strFile)
+{
+  return strFile.Left(7).Equals("bluray:");
+}
+
+bool URIUtils::IsAndroidApp(const CStdString &path)
+{
+  return path.Left(11).Equals("androidapp:");
+}
+
 bool URIUtils::IsDOSPath(const CStdString &path)
 {
   if (path.size() > 1 && path[1] == ':' && isalpha(path[0]))
@@ -318,14 +605,4 @@ bool URIUtils::IsDOSPath(const CStdString &path)
     return true;
 
   return false;
-}
-
-bool URIUtils::IsURL(const CStdString& strFile)
-{
-  return strFile.Find("://") >= 0;
-}
-
-bool URIUtils::IsStack(const CStdString& strFile)
-{
-  return strFile.Left(6).Equals("stack:");
 }
--- utils/URIUtils.h
+++ utils/URIUtils.h
@@ -29,9 +29,55 @@ public:
 
 	static CStdString GetExtension(const CStdString& strFileName);
 
+	static void Split(const CStdString& strFileNameAndPath,
+			CStdString& strPath, CStdString& strFileName);
+	static CStdStringArray SplitPath(const CStdString& strPath);
+
+	static void GetCommonPath(CStdString& strPath, const CStdString& strPath2);
+
+	static bool IsAddonsPath(const CStdString& strFile);
+	static bool IsSourcesPath(const CStdString& strFile);
+	static bool IsCDDA(const CStdString& strFile);
+	static bool IsDAAP(const CStdString& strFile);
+	static bool IsDAV(const CStdString& strFile);
 	static bool IsDOSPath(const CStdString &path);
-	static bool IsURL(const CStdString& strFile);
+	static bool IsDVD(const CStdString& strFile);
+	static bool IsFTP(const CStdString& strFile);
+	static bool IsHD(const CStdString& strFileName);
+	static bool IsHDHomeRun(const CStdString& strFile);
+	static bool IsSlingbox(const CStdString& strFile);
+	static bool IsHTSP(const CStdString& strFile);
+	static bool IsInArchive(const CStdString& strFile);
+	static bool IsInRAR(const CStdString& strFile);
+	static bool IsInAPK(const CStdString& strFile);
+	static bool IsInZIP(const CStdString& strFile);
+	static bool IsISO9660(const CStdString& strFile);
+	static bool IsLiveTV(const CStdString& strFile);
+	static bool IsPVRRecording(const CStdString& strFile);
+	static bool IsMultiPath(const CStdString& strPath);
+	static bool IsMusicDb(const CStdString& strFile);
+	static bool IsMythTV(const CStdString& strFile);
+	static bool IsNfs(const CStdString& strFile);
+	static bool IsAfp(const CStdString& strFile);
+	static bool IsOnDVD(const CStdString& strFile);
+	static bool IsOnLAN(const CStdString& strFile);
+	static bool IsPlugin(const CStdString& strFile);
+	static bool IsScript(const CStdString& strFile);
+	static bool IsRAR(const CStdString& strFile);
+	static bool IsRemote(const CStdString& strFile);
+	static bool IsSmb(const CStdString& strFile);
+	static bool IsSpecial(const CStdString& strFile);
 	static bool IsStack(const CStdString& strFile);
+	static bool IsTuxBox(const CStdString& strFile);
+	static bool IsUPnP(const CStdString& strFile);
+	static bool IsURL(const CStdString& strFile);
+	static bool IsVideoDb(const CStdString& strFile);
+	static bool IsVTP(const CStdString& strFile);
+	static bool IsAPK(const CStdString& strFile);
+	static bool IsZIP(const CStdString& strFile);
+	static bool IsArchive(const CStdString& strFile);
+	static bool IsBluray(const CStdString& strFile);
+	static bool IsAndroidApp(const CStdString& strFile);
 
 	static void AddSlashAtEnd(CStdString& strFolder);
 	static bool HasSlashAtEnd(const CStdString& strFile, bool checkURL = false);
