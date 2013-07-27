13c1533a7d03a5db82f2679b188253d4179adb5a
Zachary Seymour <adamsey16@gmail.com>
2013-07-27 11:32:56 -0400

    Created class for fetching all object details at once.

---
+++ ObjectInfoTag.cpp
@@ -0,0 +1,22 @@
+/*
+ * ObjectInfoTag.cpp
+ *
+ *  Created on: Jul 27, 2013
+ *      Author: zachary
+ */
+
+#include "ObjectInfoTag.h"
+
+CObjectInfoTag::CObjectInfoTag() {
+	m_playCount = 0;
+	m_duration = 0;
+	m_profileId = 1;
+	m_parentPathId = 0;
+	m_idObject = 0;
+
+}
+
+CObjectInfoTag::~CObjectInfoTag() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ObjectInfoTag.h
@@ -0,0 +1,50 @@
+/*
+ * ObjectInfoTag.h
+ *
+ *  Created on: Jul 27, 2013
+ *      Author: zachary
+ */
+
+#ifndef OBJECTINFOTAG_H_
+#define OBJECTINFOTAG_H_
+
+#include "utils/StdString.h"
+#include "Attribute.h"
+#include "Relationship.h"
+#include "Bookmark.h"
+#include "utils/StreamDetails.h"
+#include "settings/VideoSettings.h"
+#include "XBDateTime.h"
+
+
+class CObjectInfoTag {
+public:
+	CObjectInfoTag();
+	virtual ~CObjectInfoTag();
+
+	int m_idObject;
+	int m_profileId;
+
+	CStdString m_basePath;
+	int m_parentPathId;
+	CStdString m_strFile;
+	CStdString m_strPath;
+	CStdString m_fileNameAndPath;
+	CStdString m_url;
+
+	AttributeList m_attributes;
+	std::vector<CRelationship> m_relations;
+
+	CDateTime m_dateAdded;
+	CBookmark m_resumePoint;
+	CStreamDetails m_streams;
+	CVideoSettings m_settings;
+
+	int m_playCount;
+
+	int m_duration;
+
+
+};
+
+#endif /* OBJECTINFOTAG_H_ */
